10461c270166364dd993c9a34cd03f79d48ceb6e
rukkyme <communicatejoy@yahoo.com>
2024-04-16 15:48:47 +0100

    migrations done

--- littlelemon/Restaurant/models.py
+++ littlelemon/Restaurant/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField(default=datetime.now)
+    
+    def __str__(self):
+        return f"{self.name} - Guests {self.no_of_guests} \n {self.booking_date}"
+    
+
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    inventory = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.title} - price: {self.price}"
+    
--- littlelemon/Restaurant/urls.py
+++ littlelemon/Restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from django.contrib import admin
+from .import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    #path('', views.sayhello, name='sayhello'),
 ]
